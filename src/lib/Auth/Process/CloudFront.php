5913757ad6dc39bf1b2946a58a57a8dc8e13f69b
astclair <astclair@ecrs.com>
2018-08-04 18:39:01 -0400

    First version of CloudFront cookie module that is functional

--- src/lib/Auth/Process/CloudFront.php
+++ src/lib/Auth/Process/CloudFront.php
@@ -1,24 +1,113 @@
 <?php
 
-namespace SimpleSAML\Module\cloudfront;
+namespace SimpleSAML\Module\cloudfront\Auth\Process;
 
 class CloudFront extends \SimpleSAML\Auth\ProcessingFilter
 {
+    private $keyPairId;
+    private $privateKeyFile;
+    private $url;
+    private $useCannedPolicy;
+    private $useCustomPolicy;
+    private $cookieLifetime;
+    private $cookiePath;
+    private $cookieDomain;
+    private $cookieSecure;
+    private $cookieHttpOnly;
+
     /**
      * Create a new CloudFront instance
      */
     public function __construct($config, $reserved)
     {
         parent::__construct($config, $reserved);
-        
-        error_log(print_r(['astclair' => 'Test123'], 1));
+
+        $this->keyPairId = $config['keyPairId'] ?? null;
+        $this->privateKeyFile = $config['privateKeyFile'] ?? null;
+        $this->url = $config['url'] ?? null;
+        $this->useCannedPolicy = boolval($config['useCannedPolicy'] ?? true);
+        $this->useCustomPolicy = boolval($config['useCustomPolicy'] ?? false);
+        $this->cookieLifetime = intval($config['cookieLifetime']) ?? null;
+        $this->cookiePath = $config['cookiePath'] ?? '/';
+        $this->cookieDomain = $config['cookieDomain'] ?? '';
+        $this->cookieSecure = $config['cookieSecure'] ?? true;
+        $this->cookieHttpOnly = $config['cookieHttpOnly'] ?? true;
     }
-    
+
     public function process(&$request)
     {
-        // Do processing
-        
+        if (!empty($this->keyPairId) && !empty($this->privateKeyFile))
+        {
+            $cookieSigner = new \Aws\CloudFront\CookieSigner($this->keyPairId, $this->privateKeyFile);
+
+            try
+            {
+                // Use assertion expiration time if no lifetime is provided
+                if (is_null($this->cookieLifetime))
+                {
+                    $this->cookieLifetime = $this->getSessionLifetime();
+                }
+
+                // If cookie lifetime = 0 then the cookie will expire with the browser.
+                // So, the policy will be generated without an expiration time.
+                $lifetime = $this->cookieLifetime === 0 ? null : $this->cookieLifetime;
+
+                $expiration = new \DateTime();
+                $expiration->add(new \DateInterval(sprintf('PT%dS', $lifetime)));
+                $timestamp = $expiration->getTimestamp();
+
+                $signedCookies = $cookieSigner->getSignedCookie($this->url, $timestamp, $this->getPolicy($timestamp));
+
+                $this->setCookies($signedCookies, $timestamp);
+            }
+            catch (\InvalidArgumentException $ex)
+            {
+                return false;
+            }
+            finally
+            {
+                $cookieSigner = null;
+            }
+        }
+
         return true;
+    }
+
+    private function getPolicy($timestamp)
+    {
+        return $this->useCannedPolicy ? $this->getCannedPolicy($timestamp) : $this->getCustomPolicy($timestamp);
+    }
+
+    private function getCannedPolicy($timestamp)
+    {
+        $policy = ['Statement' => [['Resource' => $this->url]]];
+
+        if (!is_null($timestamp))
+        {
+            $policy['Statement'][0]['Condition'] = ['DateLessThan' => ['AWS:EpochTime' => $timestamp]];
+        }
+
+        return json_encode($policy, JSON_UNESCAPED_SLASHES);
+    }
+
+    private function getCustomPolicy($timestamp)
+    {
+
+    }
+
+    private function setCookies($cookies, $timestamp)
+    {
+        foreach ($cookies as $name => $value)
+        {
+            setcookie($name, $value, $timestamp, $this->cookiePath,
+                $this->cookieDomain, $this->cookieSecure, $this->cookieHttpOnly);
+        }
+    }
+
+    private function getSessionLifetime()
+    {
+        $sessionHandler = \SimpleSAML\SessionHandler::getSessionHandler();
+        return intval($sessionHandler->getCookieParams()['lifetime'] ?? 0);
     }
 }
 
