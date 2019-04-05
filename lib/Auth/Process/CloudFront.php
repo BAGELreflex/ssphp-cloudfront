aac6d9a96c6c852344639e5390f6b226537e9bab
Aaron St. Clair <astclair@ecrs.com>
2019-04-05 09:36:01 -0400

    Handled session expiration much more appropriately

--- lib/Auth/Process/CloudFront.php
+++ lib/Auth/Process/CloudFront.php
@@ -3,12 +3,12 @@ namespace SimpleSAML\Module\cloudfront\Auth\Process;
 
 /**
  * Amazon CloudFront Authentication Processing Filter
- * 
+ *
  * Generate CloudFront signed cookies, which allow you to control who can access
  * your content. Signed cookies are more practical than signed url's when your
  * end-user needs access to multiple restricted files, such as subscription-based
  * downloads, monthly news content or premium video content.
- * 
+ *
  * @author Aaron St. Clair <astclair2010@gmail.com>
  * @license https://github.com/BAGELreflex/ssphp-cloudfront/LICENSE.md
  * @version 1.0.0
@@ -25,14 +25,14 @@ class CloudFront extends \SimpleSAML\Auth\ProcessingFilter
      * "trusted signer".
      */
     private $keyPairId;
-    
+
     /**
      * @var string The fully qualified path name to private key file created for
      * the CloudFront key pair passed in for $keyPairId. This file must be
      * located on the local file system.
      */
     private $privateKeyFile;
-    
+
     /**
      * @var string The PEM-encoded private key created for the CloudFront key
      * pair passed in for $keyPairId. This private key must be base64 encoded.
@@ -42,21 +42,21 @@ class CloudFront extends \SimpleSAML\Auth\ProcessingFilter
      * memory or to access environment variables. This is a more secure method
      * of passing secure data to your application than allowing it to read a
      * private file directly.
-     * 
+     *
      * @example Apache provides the "SetEnv" directive, which will allow you to
      * base64 encode your private key file, then store the value in your
      * apache configuration, which is read by the service daemon instead of the
      * apache user.
-     * 
+     *
      * @todo Implement this
      */
     private $privateKeyBase64;
-    
+
     /**
      * @var string The base URL, including your query strings (if any), to the
      * CloudFront resource(s) for which the cookies should be authorized.
      * @see https://docs.aws.amazon.com/AmazonCloudFront/latest/DeveloperGuide/private-content-creating-signed-url-custom-policy.html#private-content-custom-policy-statement-values
-     * 
+     *
      * @todo Make this optional (If you omit the Resource parameter for a web
      * distribution, users can access all of the objects associated with any
      * distribution that is associated with the key pair that you use to create
@@ -64,28 +64,29 @@ class CloudFront extends \SimpleSAML\Auth\ProcessingFilter
      * @see https://docs.aws.amazon.com/AmazonCloudFront/latest/DeveloperGuide/private-content-creating-signed-url-custom-policy.html#private-content-custom-policy-statement-values
      */
     private $url;
-    
+
     /**
      * @var boolean Indicates that the cookies should use a Canned Policy.
      * @see https://docs.aws.amazon.com/AmazonCloudFront/latest/DeveloperGuide/private-content-signed-cookies.html#private-content-choosing-canned-custom-cookies
      */
     private $useCannedPolicy;
-    
+
     /**
      * @var boolean|null Indicates that the cookies should use a Custom Policy.
      * @see https://docs.aws.amazon.com/AmazonCloudFront/latest/DeveloperGuide/private-content-signed-cookies.html#private-content-choosing-canned-custom-cookies
-     * 
+     *
      * @todo Implement custom policy support
      */
     private $useCustomPolicy;
-    
+
     /**
-     * @var integer|null The number of seconds for which the cookies should be
-     * valid. If this value is not provided the the cookies are created with the
-     * same lifetime as the user's SAML assertion cookies.
+     * @var integer|null The number of seconds added to date() and set as the unix timestamp
+     * for the "CloudFront-Expires" cookie. If this value is not provided the the cookies are
+     * created with the same lifetime as the user's SAML assertion cookies. If the SAML
+     * assertion does not have an expiration then DEFAULT_LIFETIME_SECONDS is used.
      */
-    private $cookieLifetime;
-    
+    private $cloudFrontExpires;
+
     /**
      * @var string|null Indicates a URL path that must exist in the requested
      * URL in order to send cookie header. This is useful if your CloudFront
@@ -98,7 +99,7 @@ class CloudFront extends \SimpleSAML\Auth\ProcessingFilter
      * @see https://developer.mozilla.org/en-US/docs/Web/HTTP/Cookies#Scope_of_cookies
      */
     private $cookiePath;
-    
+
     /**
      * @var string|null Indicates the allowed hosts which can receive the
      * cookies. If a domain is not provided the the cookie is created using the
@@ -106,7 +107,7 @@ class CloudFront extends \SimpleSAML\Auth\ProcessingFilter
      * @see https://developer.mozilla.org/en-US/docs/Web/HTTP/Cookies#Scope_of_cookies
      */
     private $cookieDomain;
-    
+
     /**
      * @var boolean|null Indicates if the cookies should only be transferred
      * over HTTPS. If this value is not provided then the cookies are created
@@ -114,7 +115,7 @@ class CloudFront extends \SimpleSAML\Auth\ProcessingFilter
      * @see https://developer.mozilla.org/en-US/docs/Web/HTTP/Cookies#Secure_and_HttpOnly_cookies
      */
     private $cookieSecure;
-    
+
     /**
      * @var boolean|null Indicates if the cookies should be available to
      * JavaScript's Document.cookie API. A cookie set to HttpOnly will NOT be
@@ -123,18 +124,19 @@ class CloudFront extends \SimpleSAML\Auth\ProcessingFilter
      * flag.
      */
     private $cookieHttpOnly;
-    
+
+    const DEFAULT_LIFETIME_SECONDS = 86400;
     const MODULE_ALIAS = 'SSPHP-CloudFront';
-    
+
     /**
      * Initialize CloudFront authentication processing filter
-     * 
+     *
      * Validates and parses the configuration
-     * 
+     *
      * @param array $config Configuration needed to setup the CloudFront
      * authentication processing filter.
      * @param mixed $reserved For future use
-     * 
+     *
      * @throws \SimpleSAML\Error\Exception if the configuration is not valid.
      */
     public function __construct($config, $reserved)
@@ -145,19 +147,28 @@ class CloudFront extends \SimpleSAML\Auth\ProcessingFilter
         $this->keyPairId = $this->requireConfig($config, 'keyPairId', 'STRING');
         $this->privateKeyFile = $this->requireConfig($config, 'privateKeyFile', 'STRING');
         $this->url = $this->requireConfig($config, 'url', 'STRING');
-        
+
         $this->useCannedPolicy = boolval($config['useCannedPolicy'] ?? true);
         $this->useCustomPolicy = boolval($config['useCustomPolicy'] ?? false);
-        $this->cookieLifetime = $config['cookieLifetime'] ?? null;
+
+        $cloudFrontExpiresDateTime = new \DateTime();
+        $cloudFrontExpiresDateTime->add(
+            new \DateInterval(sprintf(
+                'PT%dS'
+                , $config['cloudFrontExpires'] ?? $this->getSessionLifetime() ?? self::DEFAULT_LIFETIME_SECONDS
+            ))
+        );
+        $this->cloudFrontExpires = $cloudFrontExpiresDateTime->getTimestamp();
+
         $this->cookiePath = $config['cookiePath'] ?? '/';
         $this->cookieDomain = $config['cookieDomain'] ?? '';
         $this->cookieSecure = $config['cookieSecure'] ?? true;
         $this->cookieHttpOnly = $config['cookieHttpOnly'] ?? true;
     }
-    
+
     /**
      * Process the CloudFront authentication response
-     * 
+     *
      * @param array $request The request
      * @return void
      */
@@ -167,29 +178,10 @@ class CloudFront extends \SimpleSAML\Auth\ProcessingFilter
 
         try
         {
-            // Use assertion expiration time if no lifetime is provided
-            if (is_null($this->cookieLifetime))
-            {
-                $this->cookieLifetime = $this->getSessionLifetime();
-            }
-            
-            // If cookie lifetime = 0 then the cookie will expire with the browser.
-            // So, the policy will be generated without an expiration time.
-            $lifetime = intval($this->cookieLifetime) === 0 ? null : intval($this->cookieLifetime);
-            $this->cookieLifetime = $lifetime;
-            
-            $expiration = new \DateTime();
-            $expiration->add(new \DateInterval(sprintf('PT%dS', $lifetime)));
-            $timestamp = $expiration->getTimestamp();
-            
-            $policy = $this->getPolicy($timestamp);
-
-            Logger::debug(sprintf('%s: cookieLifetime: %d', self::MODULE_ALIAS, $this->cookieLifetime));            
+            $policy = $this->getPolicy($this->cloudFrontExpires);
             Logger::debug(sprintf('%s: policy: %s', self::MODULE_ALIAS, print_r($policy, 1)));
 
-            $signedCookies = $cookieSigner->getSignedCookie($this->url, $timestamp, $policy);
-
-            $this->setCookies($signedCookies);
+            $this->setCookies($cookieSigner->getSignedCookie($this->url, $this->cloudFrontExpires, $policy));
         }
         catch (\InvalidArgumentException $ex)
         {
@@ -200,24 +192,24 @@ class CloudFront extends \SimpleSAML\Auth\ProcessingFilter
         return true;
     }
 
-    private function getPolicy($timestamp)
+    private function getPolicy($cloudFrontExpires)
     {
-        return $this->useCannedPolicy ? $this->getCannedPolicy($timestamp) : $this->getCustomPolicy($timestamp);
+        return $this->useCannedPolicy ? $this->getCannedPolicy($cloudFrontExpires) : $this->getCustomPolicy($cloudFrontExpires);
     }
 
-    private function getCannedPolicy($timestamp)
+    private function getCannedPolicy($cloudFrontExpires)
     {
         $policy = ['Statement' => [['Resource' => $this->url]]];
 
-        if (!is_null($timestamp))
+        if (!is_null($cloudFrontExpires))
         {
-            $policy['Statement'][0]['Condition'] = ['DateLessThan' => ['AWS:EpochTime' => $timestamp]];
+            $policy['Statement'][0]['Condition'] = ['DateLessThan' => ['AWS:EpochTime' => $cloudFrontExpires]];
         }
 
         return json_encode($policy, JSON_UNESCAPED_SLASHES);
     }
 
-    private function getCustomPolicy($timestamp)
+    private function getCustomPolicy($cloudFrontExpires)
     {
 
     }
@@ -230,7 +222,7 @@ class CloudFront extends \SimpleSAML\Auth\ProcessingFilter
             , 'domain' => $this->cookieDomain
             , 'secure' => $this->cookieSecure
             , 'httponly' => $this->cookieHttpOnly
-            , 'lifetime' => $this->cookieLifetime
+            , 'expire' => $this->cloudFrontExpires
         ];
 
         foreach ($cookies as $name => $value)
@@ -245,7 +237,7 @@ class CloudFront extends \SimpleSAML\Auth\ProcessingFilter
         $sessionHandler = \SimpleSAML\SessionHandler::getSessionHandler();
         return intval($sessionHandler->getCookieParams()['lifetime'] ?? 0);
     }
-    
+
     private function requireConfig($config, $key, $type)
     {
         if (!array_key_exists($key, $config))
@@ -257,7 +249,7 @@ class CloudFront extends \SimpleSAML\Auth\ProcessingFilter
                 , var_export($config[$key], true)
             ));
         }
-        
+
         switch ($type)
         {
             case 'INT':
@@ -304,7 +296,7 @@ class CloudFront extends \SimpleSAML\Auth\ProcessingFilter
                     ));
                 }
         }
-        
+
         return $config[$key];
     }
 }
